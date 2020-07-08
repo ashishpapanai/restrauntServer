d528001fd6a111f61be8aa2f4278121e7f8c6f5f
Ashish Papanai <ashishpapanai00@gmail.com>
2020-07-08 16:09:09 +0530

    oAuth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '706891720150302',
+        clientSecret: '9a6da5ae4954441a8e2fda571985d585'
+    }
 }
